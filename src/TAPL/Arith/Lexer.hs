73d1578ce9de2c9059772b71224b931fa0380f42
Андрей Зиненко <zinenkoan@yahoo.com>
2018-12-09 13:17:28 +0300

    Update arith

---
+++ src/TAPL/Arith/Lexer.hs
@@ -0,0 +1,30 @@
+module TAPL.Arith.Lexer where
+
+import Text.Parsec (letter, alphaNum, lower, oneOf, (<|>))
+import Text.ParserCombinators.Parsec.Language (emptyDef)
+import qualified Text.ParserCombinators.Parsec.Token as Token
+
+languageDefinition = emptyDef {
+    Token.commentStart    = "/*",
+    Token.commentEnd      = "*/",
+    Token.commentLine     = "//",
+    Token.identStart      = lower,
+    Token.identLetter     = (alphaNum <|> oneOf "?_"),
+    Token.reservedNames   = [
+        "true",
+        "false",
+        "zero?",
+        "succ",
+        "pred",
+        "zero",
+        "if",
+        "then",
+        "else"
+    ]
+}
+
+lexer = Token.makeTokenParser languageDefinition
+
+reserved   = Token.reserved   lexer
+parens     = Token.parens     lexer
+semi       = Token.semi       lexer
--- test/TAPL/ArithSpec.hs
+++ test/TAPL/ArithSpec.hs
@@ -1,105 +1,37 @@
 module TAPL.ArithSpec where
 
 import Test.Hspec
-import TAPL.Arith
+import TAPL.Arith.Evaluator (eval)
 
 spec :: Spec
 spec = do
   describe "eval" $ do
-    it "true -> true" $ do
-        eval "true" `shouldBe` "true"
+    describe "primitive values" $ do
+       let test = (\(x,y) -> it x $ do { eval x "<stdin>" `shouldBe` Right y })
+           examples = [
+              ("true", "true"),
+              ("false", "false"),
+              ("zero", "zero")
+            ]
+       mapM_ test examples
 
-    it "(true) -> true" $ do
-        eval "(true)" `shouldBe` "true"
+    describe "condition" $ do
+       let test = (\(x,y) -> it x $ do { eval x "<stdin>" `shouldBe` Right y })
+           examples = [
+              ("if true then true else false", "true"),
+              ("if true then false else false", "false"),
+              ("if false then false else true", "true")
+            ]
+       mapM_ test examples
 
-    it "((true)) -> true" $ do
-        eval "((true))" `shouldBe` "true"
-
-    it "false -> false" $ do
-        eval "false" `shouldBe` "false"
-
-    it "(false) -> false" $ do
-        eval "(false)" `shouldBe` "false"
-
-    it "((false)) -> false" $ do
-        eval "((false))" `shouldBe` "false"
-
-    it "0 -> 0" $ do
-        eval "0" `shouldBe` "0"
-
-    it "(0) -> 0" $ do
-        eval "(0)" `shouldBe` "0"
-
-    it "((0)) -> 0" $ do
-        eval "((0))" `shouldBe` "0"
-
-    it "succ 0 -> succ 0" $ do
-        eval "succ 0" `shouldBe` "succ 0"
-
-    it "(succ 0) -> succ 0" $ do
-        eval "(succ 0)" `shouldBe` "succ 0"
-
-    it "((succ 0)) -> succ 0" $ do
-        eval "((succ 0))" `shouldBe` "succ 0"
-
-    it "isZero 0 -> true" $ do
-        eval "isZero 0" `shouldBe` "true"
-
-    it "(isZero 0) -> true" $ do
-        eval "(isZero 0)" `shouldBe` "true"
-
-    it "((isZero 0)) -> true" $ do
-        eval "((isZero 0))" `shouldBe` "true"
-
-    it "((isZero ((0)))) -> true" $ do
-        eval "((isZero ((0))))" `shouldBe` "true"
-
-    it "isZero (succ 0) -> false" $ do
-        eval "isZero (succ 0)" `shouldBe` "false"
-
-    it "pred (succ 0) -> 0" $ do
-        eval "pred (succ 0)" `shouldBe` "0"
-
-    it "pred succ 0 -> 0" $ do
-        eval "pred succ 0" `shouldBe` "0"
-
-    it "pred succ (0) -> 0" $ do
-        eval "pred succ (0)" `shouldBe` "0"
-
-    it "succ (pred (succ (0))) -> succ 0" $ do
-        eval "succ (pred (succ (0)))" `shouldBe` "succ 0"
-
-    it "succ (pred succ 0) -> succ 0" $ do
-        eval "succ (pred succ 0)" `shouldBe` "succ 0"
-
-    it "pred 0 -> 0" $ do
-        eval "pred 0" `shouldBe` "0"
-
-    it "succ pred pred pred pred 0 -> succ 0" $ do
-        eval "succ pred pred pred pred 0" `shouldBe` "succ 0"
-
-    it "succ (pred pred pred pred (0)) -> succ 0" $ do
-        eval "succ (pred pred pred pred (0))" `shouldBe` "succ 0"
-
-    it "if isZero 0 then succ 0 else succ pred 0 -> succ 0" $ do
-        eval "if isZero 0 then succ 0 else succ pred 0" `shouldBe` "succ 0"
-
-    it "if isZero succ 0 then succ 0 else pred 0 -> 0" $ do
-        eval "if isZero succ 0 then succ 0 else pred 0" `shouldBe` "0"
-
-    it "if isZero (succ 0) then succ 0 else 0 -> 0" $ do
-        eval "if isZero (succ 0) then succ 0 else 0" `shouldBe` "0"
-
-    it "if (succ 0) then succ 0 else 0 -> if succ 0 then succ 0 else 0" $ do
-        eval "if (succ 0) then succ 0 else 0" `shouldBe` "if succ 0 then succ 0 else 0"
-
-    it "if pred (succ 0) then succ 0 else 0 -> if 0 then succ 0 else 0" $ do
-        eval "if pred (succ 0) then succ 0 else 0" `shouldBe` "if 0 then succ 0 else 0"
-
-    it "if isZero (pred (succ 0)) then succ 0 else 0 -> succ 0" $ do
-        eval "if isZero (pred (succ 0)) then succ 0 else 0" `shouldBe` "succ 0"
-
-    it "if (isZero (pred (succ 0)) then succ 0 else 0 -> fail" $ do
-        eval "if (isZero (pred (succ 0)) then succ 0 else 0" `shouldBe` "\"arith\" (line 1, column 28):\nunexpected \"t\"\nexpecting space, white space or \")\""
-
-
+    describe "predefined functions" $ do
+        let test = (\(x,y) -> it x $ do { eval x "<stdin>" `shouldBe` Right y })
+            examples = [
+              ("succ zero", "succ zero"),
+              ("pred zero", "zero"),
+              ("pred succ zero", "zero"),
+              ("succ pred pred zero", "succ zero"),
+              ("zero? zero", "true"),
+              ("zero? succ zero", "false")
+             ]
+        mapM_ test examples
