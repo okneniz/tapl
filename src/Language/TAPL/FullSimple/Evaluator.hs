175f41bf4ff35bdf5cc9d8d9c4adfb963b299470
Андрей Зиненко <zinenkoan@yahoo.com>
2020-05-08 10:32:46 +0300

    Use oneliners in Language.TAPL.FullSimple.Evaluator normalize function.

--- src/Language/TAPL/FullSimple/Evaluator.hs
+++ src/Language/TAPL/FullSimple/Evaluator.hs
@@ -32,50 +32,30 @@ fullNormalize t = case normalize t of
 normalize :: Term -> Maybe Term
 normalize (TIf _ (TTrue _) t _) = return t
 normalize (TIf _ (TFalse _) _ t) = return t
-
-normalize (TIf info t1 t2 t3) = do
-    t1' <- normalize t1
-    return $ TIf info t1' t2 t3
+normalize (TIf info t1 t2 t3) = normalize t1 >>= \t1' -> return $ TIf info t1' t2 t3
 
 normalize (TApp _ (TAbs _ _ _ t) v) | isVal v = return $ substitutionTop v t
+normalize (TApp info t1 t2) | isVal t1 = TApp info t1 <$> normalize t2
+normalize (TApp info t1 t2) = normalize t1 >>= \t1' -> return $ TApp info t1' t2
 
-normalize (TApp info t1 t2) | isVal t1 = do
-    t2' <- normalize t2
-    return $ TApp info t1 t2'
-
-normalize (TApp info t1 t2) = do
-    t1' <- normalize t1
-    return $ TApp info t1' t2
-
-normalize (TSucc info t) = do
-    t' <- normalize t
-    return $ TSucc info t'
+normalize (TSucc info t) = TSucc info <$> normalize t
 
 normalize (TPred _ (TZero info)) = return $ TZero info
 normalize (TPred _ (TSucc _ t)) | isNumerical t = return t
-normalize (TPred info t) = do
-    t' <- normalize t
-    return $ TPred info t'
+normalize (TPred info t) = TPred info <$> normalize t
 
 normalize (TIsZero _ (TZero info)) = return $ TTrue info
 normalize (TIsZero _ (TSucc info t)) | isNumerical t = return $ TFalse info
-normalize (TIsZero info t) = do
-    t' <- normalize t
-    return $ TIsZero info t'
+normalize (TIsZero info t) = TIsZero info <$> normalize t
 
-normalize (TPair info t1 t2) | isVal t1 = do
-    t2' <- normalize t2
-    return $ TPair info t1 t2'
-
-normalize (TPair info t1 t2) = do
-    t1' <- normalize t1
-    return $ TPair info t1' t2
+normalize (TPair info t1 t2) | isVal t1 = TPair info t1 <$> normalize t2
+normalize (TPair info t1 t2) = normalize t1 >>= \t1' -> return $ TPair info t1' t2
 
 normalize (TRecord _ fields) | (Map.size fields) == 0 = Nothing
 normalize t@(TRecord _ _) | isVal t = Nothing
 
 normalize (TRecord info fields) = do
-    fields' <- sequence $ evalField <$> Map.toList fields -- не то!
+    fields' <- sequence $ evalField <$> Map.toList fields
     return $ TRecord info (Map.fromList fields')
     where evalField (k, field) | isVal field = return (k, field)
           evalField (k, field) = do
@@ -83,7 +63,6 @@ normalize (TRecord info fields) = do
             return (k, field')
 
 normalize (TLookup _ t@(TRecord _ fields) (TKeyword _ key)) | isVal t = Map.lookup key fields
-
 normalize (TLookup info t@(TRecord _ _) (TKeyword x key)) = do
     t' <- normalize t
     return $ (TLookup info t' (TKeyword x key))
@@ -91,94 +70,19 @@ normalize (TLookup info t@(TRecord _ _) (TKeyword x key)) = do
 normalize (TLookup _ (TPair _ t _) (TInt _ 0)) | isVal t = return t
 normalize (TLookup _ (TPair _ _ t) (TInt _ 1)) | isVal t = return t
 
-
-normalize (TLookup info t k) = do
-    t' <- normalize t
-    return $ TLookup info t' k
+normalize (TLookup info t k) = normalize t >>= \t' -> return $ TLookup info t' k
 
 normalize (TLet _ _ t1 t2) | isVal t1 = return $ substitutionTop t1 t2
-
-normalize (TLet info v t1 t2) = do
-    t1' <- normalize t1
-    return $ TLet info v t1' t2
+normalize (TLet info v t1 t2) = normalize t1 >>= \t1' -> return $ TLet info v t1' t2
 
 normalize (TAscribe _ t _) = return t
 
-normalize t1@(TFix _ a@(TAbs _ _ _ t2)) | isVal a = do
-    return $ substitutionTop t1 t2
-
-normalize (TFix info t) = do
-    t' <- normalize t
-    return $ TFix info t'
+normalize t1@(TFix _ a@(TAbs _ _ _ t2)) | isVal a = return $ substitutionTop t1 t2
+normalize (TFix info t) = TFix info <$> normalize t
 
 normalize (TCase _ (TTag _ key v _) branches) | isVal v =
     liftM (\(_, t) -> substitutionTop v t)
           (Map.lookup key branches)
 
-normalize (TCase info t fields) = do
-    t' <- normalize t
-    return $ TCase info t' fields
-
+normalize (TCase info t fields) = normalize t >>= \t' -> return $ TCase info t' fields
 normalize _ = Nothing
-
-isVal :: Term -> Bool
-isVal (TTrue _) = True
-isVal (TFalse _) = True
-isVal (TAbs _ _ _ _) = True
-isVal (TString _ _) = True
-isVal (TUnit _) = True
-isVal (TFloat _ _) = True
-isVal (TPair _ t1 t2) = (isVal t1) && (isVal t2)
-isVal x | isNumerical x = True
-isVal (TRecord _ ts) = all isVal $ Map.elems ts
-isVal (TAscribe _ t _) = isVal t
-isVal (TTag _ _ t _) = isVal t
-isVal _ = False
-
-isNumerical :: Term -> Bool
-isNumerical (TZero _) = True
-isNumerical (TSucc _ x) = isNumerical x
-isNumerical _ = False
-
-tmmap :: (Int -> Info -> Depth -> VarName -> Term) -> Int -> Term -> Term
-tmmap onvar s t = walk s t
-            where walk c (TVar info name depth) = onvar c info name depth
-                  walk c (TAbs info x ty t1) = TAbs info x ty (walk (c+1) t1)
-                  walk c (TApp info t1 t2) = TApp info (walk c t1) (walk c t2)
-                  walk c (TIf info t1 t2 t3) = TIf info (walk c t1) (walk c t2) (walk c t3)
-                  walk _ (TTrue info) = TTrue info
-                  walk _ (TFalse info) = TFalse info
-                  walk _ (TString info x) = TString info x
-                  walk _ (TUnit info) = TUnit info
-                  walk _ (TZero info) = TZero info
-                  walk c (TIsZero info t1) = TIsZero info (walk c t1)
-                  walk c (TPred info t1) = TPred info (walk c t1)
-                  walk c (TSucc info t1) = TSucc info (walk c t1)
-                  walk _ (TFloat info t1) = TFloat info t1
-                  walk _ (TInt info t1) = TInt info t1
-                  walk c (TPair info t1 t2) = TPair info (walk c t1) (walk c t2)
-                  walk c (TRecord info fields) = TRecord info $ Map.map (walk c) fields
-                  walk c (TTag info k t1 ty) = TTag info k (walk c t1) ty
-                  walk c (TLookup info r k) = TLookup info (walk c r) k
-                  walk c (TLet info x t1 t2) = TLet info x (walk c t1) (walk (c+1) t2)
-                  walk c (TAscribe info t1 ty) = TAscribe info (walk c t1) ty
-                  walk c (TCase info t1 branches) = TCase info (walk c t1) $ Map.map walkBranch branches
-                                              where walkBranch (x, y) = (x, walk (c + 1) y)
-                  walk c (TFix info t1) = TFix info (walk c t1)
-                  walk _ t1@(TKeyword _ _) = t1
-
-termShiftAbove :: Depth -> VarName -> Term -> Term
-termShiftAbove d s t = tmmap onvar s t
-                 where onvar c info name depth | name >= c = TVar info (name + d) (depth + d)
-                       onvar _ info name depth = TVar info name (depth + d)
-
-shift :: VarName -> Term -> Term
-shift d t = termShiftAbove d 0 t
-
-substitution :: VarName -> Term -> Term -> Term
-substitution j s t = tmmap onvar 0 t
-               where onvar c _ name _ | name == j + c = shift c s
-                     onvar _ info name depth = TVar info name depth
-
-substitutionTop :: Term -> Term -> Term
-substitutionTop s t = shift (-1) (substitution 0 (shift 1 s) t)
--- src/Language/TAPL/FullSimple/Types.hs
+++ src/Language/TAPL/FullSimple/Types.hs
@@ -1,6 +1,7 @@
 module Language.TAPL.FullSimple.Types where
 
 import Data.Map (Map)
+import qualified Data.Map.Strict as Map
 
 data Term = TTrue Info
           | TFalse Info
@@ -51,3 +52,65 @@ type AST = [Term]
 data Info = Info { row :: Int, column :: Int } deriving (Show)
 
 data Binding = NameBind | VarBind Type deriving (Show)
+
+isVal :: Term -> Bool
+isVal (TTrue _) = True
+isVal (TFalse _) = True
+isVal (TAbs _ _ _ _) = True
+isVal (TString _ _) = True
+isVal (TUnit _) = True
+isVal (TFloat _ _) = True
+isVal (TPair _ t1 t2) = (isVal t1) && (isVal t2)
+isVal x | isNumerical x = True
+isVal (TRecord _ ts) = all isVal $ Map.elems ts
+isVal (TAscribe _ t _) = isVal t
+isVal (TTag _ _ t _) = isVal t
+isVal _ = False
+
+isNumerical :: Term -> Bool
+isNumerical (TZero _) = True
+isNumerical (TSucc _ x) = isNumerical x
+isNumerical _ = False
+
+tmmap :: (Int -> Info -> Depth -> VarName -> Term) -> Int -> Term -> Term
+tmmap onvar s t = walk s t
+            where walk c (TVar info name depth) = onvar c info name depth
+                  walk c (TAbs info x ty t1) = TAbs info x ty (walk (c+1) t1)
+                  walk c (TApp info t1 t2) = TApp info (walk c t1) (walk c t2)
+                  walk c (TIf info t1 t2 t3) = TIf info (walk c t1) (walk c t2) (walk c t3)
+                  walk _ (TTrue info) = TTrue info
+                  walk _ (TFalse info) = TFalse info
+                  walk _ (TString info x) = TString info x
+                  walk _ (TUnit info) = TUnit info
+                  walk _ (TZero info) = TZero info
+                  walk c (TIsZero info t1) = TIsZero info (walk c t1)
+                  walk c (TPred info t1) = TPred info (walk c t1)
+                  walk c (TSucc info t1) = TSucc info (walk c t1)
+                  walk _ (TFloat info t1) = TFloat info t1
+                  walk _ (TInt info t1) = TInt info t1
+                  walk c (TPair info t1 t2) = TPair info (walk c t1) (walk c t2)
+                  walk c (TRecord info fields) = TRecord info $ Map.map (walk c) fields
+                  walk c (TTag info k t1 ty) = TTag info k (walk c t1) ty
+                  walk c (TLookup info r k) = TLookup info (walk c r) k
+                  walk c (TLet info x t1 t2) = TLet info x (walk c t1) (walk (c+1) t2)
+                  walk c (TAscribe info t1 ty) = TAscribe info (walk c t1) ty
+                  walk c (TCase info t1 branches) = TCase info (walk c t1) $ Map.map walkBranch branches
+                                              where walkBranch (x, y) = (x, walk (c + 1) y)
+                  walk c (TFix info t1) = TFix info (walk c t1)
+                  walk _ t1@(TKeyword _ _) = t1
+
+termShiftAbove :: Depth -> VarName -> Term -> Term
+termShiftAbove d s t = tmmap onvar s t
+                 where onvar c info name depth | name >= c = TVar info (name + d) (depth + d)
+                       onvar _ info name depth = TVar info name (depth + d)
+
+shift :: VarName -> Term -> Term
+shift d t = termShiftAbove d 0 t
+
+substitution :: VarName -> Term -> Term -> Term
+substitution j s t = tmmap onvar 0 t
+               where onvar c _ name _ | name == j + c = shift c s
+                     onvar _ info name depth = TVar info name depth
+
+substitutionTop :: Term -> Term -> Term
+substitutionTop s t = shift (-1) (substitution 0 (shift 1 s) t)
