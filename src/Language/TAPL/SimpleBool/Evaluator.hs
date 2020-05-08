8b8bd4ad84b97b87f6e7a5f8e4baa8b6e012c565
Андрей Зиненко <zinenkoan@yahoo.com>
2020-05-08 10:24:23 +0300

    Use oneliners in Language.TAPL.SimpleBool.Evaluator normalize function.

--- src/Language/TAPL/SimpleBool/Evaluator.hs
+++ src/Language/TAPL/SimpleBool/Evaluator.hs
@@ -29,50 +29,8 @@ fullNormalize t = case normalize t of
 normalize :: Term -> Maybe Term
 normalize (TIf _ (TTrue _) t _ ) = return t
 normalize (TIf _ (TFalse _) _ t) = return t
-
-normalize (TIf info t1 t2 t3) = do
-  t1' <- normalize t1
-  return $ TIf info t1' t2 t3
-
+normalize (TIf info t1 t2 t3) = normalize t1 >>= \t1' -> return $ TIf info t1' t2 t3
 normalize (TApp _ (TAbs _ _ _ t) v) | isVal v = return $ substitutionTop v t
-
-normalize (TApp info t1 t2) | isVal t1  = do
-    t2' <- normalize t2
-    return $ TApp info t1 t2'
-
-normalize (TApp info t1 t2) = do
-    t1' <- normalize t1
-    return $ TApp info t1' t2
-
+normalize (TApp info t1 t2) | isVal t1 = TApp info t1 <$> normalize t2
+normalize (TApp info t1 t2) = normalize t1 >>= \t1' -> return $ TApp info t1' t2
 normalize _ =  Nothing
-
-isVal :: Term -> Bool
-isVal (TTrue _) = True
-isVal (TFalse _) = True
-isVal (TAbs _ _ _ _) = True
-isVal _ = False
-
-tmmap :: (Int -> Info -> Depth -> VarName -> Term) -> Int -> Term -> Term
-tmmap onvar s t = walk s t
-            where walk c (TVar info name depth) = onvar c info name depth
-                  walk c (TAbs info x ty t1) = TAbs info x ty (walk (c+1) t1)
-                  walk c (TApp info t1 t2) = TApp info (walk c t1) (walk c t2)
-                  walk c (TIf info t1 t2 t3) = TIf info (walk c t1) (walk c t2) (walk c t3)
-                  walk _ (TTrue info) = TTrue info
-                  walk _ (TFalse info) = TFalse info
-
-termShiftAbove :: Depth -> VarName -> Term -> Term
-termShiftAbove d s t = tmmap onvar s t
-                 where onvar c info name depth | name >= c = TVar info (name + d) (depth + d)
-                       onvar _ info name depth = TVar info name (depth + d)
-
-shift :: VarName -> Term -> Term
-shift d t = termShiftAbove d 0 t
-
-substitution :: VarName -> Term -> Term -> Term
-substitution j s t = tmmap onvar 0 t
-               where onvar c _ name _ | name == j + c = shift c s
-                     onvar _ info name depth = TVar info name depth
-
-substitutionTop :: Term -> Term -> Term
-substitutionTop s t = shift (-1) (substitution 0 (shift 1 s) t)
--- src/Language/TAPL/SimpleBool/Types.hs
+++ src/Language/TAPL/SimpleBool/Types.hs
@@ -26,3 +26,34 @@ instance Show Info where
 instance Show Type where
     show (TyArrow t1 t2) = "("++ show t1 ++ " -> " ++ show t2 ++ ")"
     show TyBool = "Bool"
+
+isVal :: Term -> Bool
+isVal (TTrue _) = True
+isVal (TFalse _) = True
+isVal (TAbs _ _ _ _) = True
+isVal _ = False
+
+tmmap :: (Int -> Info -> Depth -> VarName -> Term) -> Int -> Term -> Term
+tmmap onvar s t = walk s t
+            where walk c (TVar info name depth) = onvar c info name depth
+                  walk c (TAbs info x ty t1) = TAbs info x ty (walk (c+1) t1)
+                  walk c (TApp info t1 t2) = TApp info (walk c t1) (walk c t2)
+                  walk c (TIf info t1 t2 t3) = TIf info (walk c t1) (walk c t2) (walk c t3)
+                  walk _ (TTrue info) = TTrue info
+                  walk _ (TFalse info) = TFalse info
+
+termShiftAbove :: Depth -> VarName -> Term -> Term
+termShiftAbove d s t = tmmap onvar s t
+                 where onvar c info name depth | name >= c = TVar info (name + d) (depth + d)
+                       onvar _ info name depth = TVar info name (depth + d)
+
+shift :: VarName -> Term -> Term
+shift d t = termShiftAbove d 0 t
+
+substitution :: VarName -> Term -> Term -> Term
+substitution j s t = tmmap onvar 0 t
+               where onvar c _ name _ | name == j + c = shift c s
+                     onvar _ info name depth = TVar info name depth
+
+substitutionTop :: Term -> Term -> Term
+substitutionTop s t = shift (-1) (substitution 0 (shift 1 s) t)
