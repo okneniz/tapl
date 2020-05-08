673f4000d03c414e85541c6d33a386af2c14a351
Андрей Зиненко <zinenkoan@yahoo.com>
2020-05-08 10:20:49 +0300

    Use oneliners in Language.TAPL.TypedArith.Evaluator normalize function.

--- src/Language/TAPL/TypedArith/Evaluator.hs
+++ src/Language/TAPL/TypedArith/Evaluator.hs
@@ -27,82 +27,18 @@ fullNormalize t = case normalize t of
                        Nothing -> t
 
 normalize :: Term -> Maybe Term
-
 normalize (TIf _ (TTrue _) t _ ) = return t
 normalize (TIf _ (TFalse _) _ t) = return t
-
-normalize (TIf info t1 t2 t3) = do
-  t1' <- normalize t1
-  return $ TIf info t1' t2 t3
-
-normalize (TApp _ (TAbs _ _ _ t) v) | isVal v = do
-    return $ substitutionTop v t
-
-normalize (TApp info t1 t2) | isVal t1  = do
-    t2' <- normalize t2
-    return $ TApp info t1 t2'
-
-normalize (TApp info t1 t2) = do
-    t1' <- normalize t1
-    return $ TApp info t1' t2
-
-normalize (TSucc info t) = do
-    t' <- normalize t
-    return $ TSucc info t'
-
+normalize (TIf info t1 t2 t3) = normalize t1 >>= \t1' -> return $ TIf info t1' t2 t3
+normalize (TApp _ (TAbs _ _ _ t) v) | isVal v = return $ substitutionTop v t
+normalize (TApp info t1 t2) | isVal t1 = TApp info t1 <$> normalize t2
+normalize (TApp info t1 t2) = normalize t1 >>= \t1' -> return $ TApp info t1' t2
+normalize (TSucc info t) = TSucc info <$> normalize t
 normalize (TPred _ (TZero info)) = return $ TZero info
 normalize (TPred _ (TSucc _ t)) | isNumerical t = return t
-
-normalize (TPred info t) = do
-    t' <- normalize t
-    return $ TPred info t'
-
+normalize (TPred info t) = TPred info <$> normalize t
 normalize (TIsZero _ (TZero info)) = return $ TTrue info
 normalize (TIsZero _ (TSucc info t)) | isNumerical t = return $ TFalse info
-
-normalize (TIsZero info t) = do
-    t' <- normalize t
-    return $ TIsZero info t'
-
+normalize (TIsZero info t) = TIsZero info <$> normalize t
 normalize _ = Nothing
 
-isVal :: Term -> Bool
-isVal (TTrue _) = True
-isVal (TFalse _) = True
-isVal (TAbs _ _ _ _) = True
-isVal x | isNumerical x = True
-isVal _ = False
-
-isNumerical :: Term -> Bool
-isNumerical (TZero _) = True
-isNumerical (TSucc _ x) = isNumerical x
-isNumerical _ = False
-
-tmmap :: (Int -> Info -> Depth -> VarName -> Term) -> Int -> Term -> Term
-tmmap onvar s t = walk s t
-            where walk c (TVar info name depth) = onvar c info name depth
-                  walk c (TAbs info x ty t1) = TAbs info x ty (walk (c+1) t1)
-                  walk c (TApp info t1 t2) = TApp info (walk c t1) (walk c t2)
-                  walk c (TIf info t1 t2 t3) = TIf info (walk c t1) (walk c t2) (walk c t3)
-                  walk _ (TTrue info) = TTrue info
-                  walk _ (TFalse info) = TFalse info
-                  walk _ (TZero info) = TZero info
-                  walk c (TIsZero info t1) = TIsZero info (walk c t1)
-                  walk c (TPred info t1) = TPred info (walk c t1)
-                  walk c (TSucc info t1) = TSucc info (walk c t1)
-
-termShiftAbove :: Depth -> VarName -> Term -> Term
-termShiftAbove d s t = tmmap onvar s t
-                 where onvar c info name depth | name >= c = TVar info (name + d) (depth + d)
-                       onvar _ info name depth = TVar info name (depth + d)
-
-shift :: VarName -> Term -> Term
-shift d t = termShiftAbove d 0 t
-
-substitution :: VarName -> Term -> Term -> Term
-substitution j s t = tmmap onvar 0 t
-               where onvar c _ name _ | name == j + c = shift c s
-                     onvar _ info name depth = TVar info name depth
-
-substitutionTop :: Term -> Term -> Term
-substitutionTop s t = shift (-1) (substitution 0 (shift 1 s) t)
--- src/Language/TAPL/TypedArith/Types.hs
+++ src/Language/TAPL/TypedArith/Types.hs
@@ -31,3 +31,45 @@ instance Show Type where
     show (TyArrow t1 t2) = "("++ show t1 ++ " -> " ++ show t2 ++ ")"
     show TyBool = "Bool"
     show TyNat = "Nat"
+
+
+isVal :: Term -> Bool
+isVal (TTrue _) = True
+isVal (TFalse _) = True
+isVal (TAbs _ _ _ _) = True
+isVal x | isNumerical x = True
+isVal _ = False
+
+isNumerical :: Term -> Bool
+isNumerical (TZero _) = True
+isNumerical (TSucc _ x) = isNumerical x
+isNumerical _ = False
+
+tmmap :: (Int -> Info -> Depth -> VarName -> Term) -> Int -> Term -> Term
+tmmap onvar s t = walk s t
+            where walk c (TVar info name depth) = onvar c info name depth
+                  walk c (TAbs info x ty t1) = TAbs info x ty (walk (c+1) t1)
+                  walk c (TApp info t1 t2) = TApp info (walk c t1) (walk c t2)
+                  walk c (TIf info t1 t2 t3) = TIf info (walk c t1) (walk c t2) (walk c t3)
+                  walk _ (TTrue info) = TTrue info
+                  walk _ (TFalse info) = TFalse info
+                  walk _ (TZero info) = TZero info
+                  walk c (TIsZero info t1) = TIsZero info (walk c t1)
+                  walk c (TPred info t1) = TPred info (walk c t1)
+                  walk c (TSucc info t1) = TSucc info (walk c t1)
+
+termShiftAbove :: Depth -> VarName -> Term -> Term
+termShiftAbove d s t = tmmap onvar s t
+                 where onvar c info name depth | name >= c = TVar info (name + d) (depth + d)
+                       onvar _ info name depth = TVar info name (depth + d)
+
+shift :: VarName -> Term -> Term
+shift d t = termShiftAbove d 0 t
+
+substitution :: VarName -> Term -> Term -> Term
+substitution j s t = tmmap onvar 0 t
+               where onvar c _ name _ | name == j + c = shift c s
+                     onvar _ info name depth = TVar info name depth
+
+substitutionTop :: Term -> Term -> Term
+substitutionTop s t = shift (-1) (substitution 0 (shift 1 s) t)
