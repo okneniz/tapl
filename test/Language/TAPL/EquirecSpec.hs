8bc2f9dc32f6a99cbf9f83c2831996c77d882840
Андрей Зиненко <zinenkoan@yahoo.com>
2020-08-14 20:50:17 +0300

    Improve spec in Equirec.

--- test/Language/TAPL/EquirecSpec.hs
+++ test/Language/TAPL/EquirecSpec.hs
@@ -1,15 +1,20 @@
 module Language.TAPL.EquirecSpec where
 
 import Test.Hspec
+import Language.TAPL.TestHelpers
 import Language.TAPL.Equirec.Evaluator (evalString)
 
 spec :: Spec
 spec = do
   describe "eval" $ do
     describe "abstractions" $ do
-      let test = (\(x,y) -> it x $ do { evalString x "<stdin>" `shouldBe` Right y })
-          examples = [
-              ("(lambda x:A.x)", "(lambda x.x):A -> A"),
-              ("(lambda f:Rec X.A->A.lambda x:A. f x)", "(lambda f.(lambda x.f x)):Rec X.A -> A -> A -> A")
+        tests evalString [
+            (
+                "(lambda x:A.x)",
+                pass "(lambda x.x):A -> A"
+            ),
+            (
+                "(lambda f:Rec X.A->A.lambda x:A. f x)",
+                pass "(lambda f.(lambda x.f x)):Rec X.A -> A -> A -> A"
+            )
            ]
-      mapM_ test examples
