5cb953338000b3bd1b5ebae2cf967e3291673aab
Андрей Зиненко <zinenkoan@yahoo.com>
2020-08-14 21:05:55 +0300

    Improve spec in FullError.

--- test/Language/TAPL/FullErrorSpec.hs
+++ test/Language/TAPL/FullErrorSpec.hs
@@ -1,8 +1,7 @@
-{-# LANGUAGE FlexibleContexts #-}
-
 module Language.TAPL.FullErrorSpec where
 
 import Test.Hspec
+import Language.TAPL.TestHelpers
 import Language.TAPL.FullError.Evaluator (evalString)
 
 eval = evalString
@@ -12,41 +11,33 @@ spec = do
   describe "eval" $ do
     describe "values" $ do
         describe "primitive values" $ do
-           let test = (\(x,y) -> it x $ do { eval x "<stdin>" `shouldBe` Right y })
-               examples = [
-                  ("true", "true:Bool"),
-                  ("false", "false:Bool"),
-                  ("error", "error:Bot")
+            tests evalString [
+                  ("true", pass "true:Bool"),
+                  ("false", pass "false:Bool"),
+                  ("error", pass "error:Bot")
                 ]
-           mapM_ test examples
 
         describe "abstractions" $ do
-            let test = (\(x,y) -> it x $ do { eval x "<stdin>" `shouldBe` Right y })
-                examples = [
-                    ("(lambda x:Bool.x)", "(lambda x.x):Bool -> Bool"),
-                    ("(lambda x:Bot.error)", "(lambda x.error):Bot -> Bot")
+            tests evalString [
+                    ("(lambda x:Bool.x)", pass "(lambda x.x):Bool -> Bool"),
+                    ("(lambda x:Bot.error)", pass "(lambda x.error):Bot -> Bot")
                  ]
-            mapM_ test examples
 
     describe "operations" $ do
         describe "condition" $ do
-           let test = (\(x,y) -> it x $ do { eval x "<stdin>" `shouldBe` Right y })
-               examples = [
-                  ("if true then false else true", "false:Bool"),
-                  ("if true then false else error", "false:Bool"),
-                  ("if true then error else true", "error:Bot"),
-                  ("if false then false else true", "true:Bool"),
-                  ("if false then error else true", "true:Bool"),
-                  ("if false then false else error", "error:Bot")
+            tests evalString [
+                  ("if true then false else true", pass "false:Bool"),
+                  ("if true then false else error", pass "false:Bool"),
+                  ("if true then error else true", pass "error:Bot"),
+                  ("if false then false else true", pass "true:Bool"),
+                  ("if false then error else true", pass "true:Bool"),
+                  ("if false then false else error", pass "error:Bot")
                 ]
-           mapM_ test examples
 
         describe "apply" $ do
-            let test = (\(x,y) -> it x $ do { eval x "<stdin>" `shouldBe` Right y })
-                examples = [
-                  ("(lambda x:Bool. if x then false else true) true", "false:Bool"),
-                  ("(lambda x:Bool.error) true", "error:Bot"),
-                  ("(lambda x:Bool.if x then error else false) true", "error:Bot"),
-                  ("(lambda x:Bool.if x then error else false) false", "false:Bool")
+            tests evalString [
+                  ("(lambda x:Bool. if x then false else true) true", pass "false:Bool"),
+                  ("(lambda x:Bool.error) true", pass "error:Bot"),
+                  ("(lambda x:Bool.if x then error else false) true", pass "error:Bot"),
+                  ("(lambda x:Bool.if x then error else false) false", pass "false:Bool")
                  ]
-            mapM_ test examples
